9628e57b420a5dc72b474f9b78c0c79676305d2d
Ashutosh619-sudo <Ashutoshsinghrkt@gmail.com>
2021-11-18 01:19:17 +0530

    packging change to war

--- src/main/java/com/helloworld/helloworld/HelloWorldApplication.java
+++ src/main/java/com/helloworld/helloworld/HelloWorldApplication.java
@@ -2,12 +2,13 @@ package com.helloworld.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class HelloWorldApplication {
+public class HelloWorldApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
